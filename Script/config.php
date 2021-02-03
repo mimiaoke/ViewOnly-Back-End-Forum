b3245425d79eaf153ec34eac6f0d6a33595bc18c
LunaHu1125 <73286246+LunaHu1125@users.noreply.github.com>
2021-02-03 17:37:11 +0000

    Initial commit

---
+++ Script/config.php
@@ -0,0 +1,24 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.wowonder.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com   
+// +------------------------------------------------------------------------+
+// | WoWonder - The Ultimate PHP Social Networking Platform
+// | Copyright (c) 2017 WoWonder. All rights reserved.
+// +------------------------------------------------------------------------+
+@header('Location: install');
+exit();
+// MySQL Hostname
+$sql_db_host = "mysql:host=localhost;port=3306;";
+// MySQL Database User
+$sql_db_user = "lunahued_forum";
+// MySQL Database Password
+$sql_db_pass = "Lu19971125";
+// MySQL Database Name
+$sql_db_name = "lunahued_forum";
+
+// Site URL
+$site_url = "site_url"; // e.g (http://example.com)
+?>
